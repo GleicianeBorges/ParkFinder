03dab4b93992f89491814b999ecc795d583be429
Gleiciane Barros Borges <gleiciane6217@DIGITALCOLLEGE.LOCAL>
2024-10-14 21:52:43 -0300

    added pages

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,11 @@
+import CardStatus from "@/components/CardStatus/CardStatus";
 import SideBar from "@/components/SideBar/SideBar";
 
 export default function DashboardPage() {
     return (
         <div className="flex relative min-h-full items-center flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <SideBar />
+            <CardStatus></CardStatus>
         </div>
     )
 }
